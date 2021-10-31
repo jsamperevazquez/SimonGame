c3748487847de6241d10e53703e8b339975fa178
JSampereVázquez <jsamperevazquez@danielcastelao.org>
2021-10-31 23:31:31 +0100

    Generados diferentes logs y toast

--- app/src/main/java/com/cursoandroid/simongame/MainActivity.kt
+++ app/src/main/java/com/cursoandroid/simongame/MainActivity.kt
@@ -2,8 +2,10 @@ package com.cursoandroid.simongame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +22,18 @@ class MainActivity : AppCompatActivity() {
         arrayBotones[1] = bVerde
         arrayBotones[2] = bAmarillo
         arrayBotones[3] = bAzul
-
+        botonEmpezar.setOnClickListener {
+            val toast = Toast.makeText(applicationContext, "Jugador juega", Toast.LENGTH_SHORT).show()
+            Log.d("estado", "Jugador ha empezado a jugar")
+        }
     }
-    fun mostrarRonda() {}
-    fun ejecutarSecuencia() {}
+    fun mostrarRonda() {
+        val toast = Toast.makeText(applicationContext, "Mostrando ronda", Toast.LENGTH_SHORT).show()
+        Log.d("ronda", "Mostrando ronda")
+    }
+    fun ejecutarSecuencia() {
+        var toast = Toast.makeText(applicationContext, "Mostrando secuencia", Toast.LENGTH_SHORT).show()
+    }
     fun comprobarSecuencia(){}
 
 }
