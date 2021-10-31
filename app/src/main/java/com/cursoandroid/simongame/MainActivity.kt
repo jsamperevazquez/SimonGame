1ba1834af0fee6dab435c7cc8225ddde18d9b8c5
JSampereVázquez <jsamperevazquez@danielcastelao.org>
2021-10-31 23:15:39 +0100

    Creados métodos principales de funcionalidad

--- app/src/main/java/com/cursoandroid/simongame/MainActivity.kt
+++ app/src/main/java/com/cursoandroid/simongame/MainActivity.kt
@@ -2,10 +2,22 @@ package com.cursoandroid.simongame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonEmpezar: Button = findViewById(R.id.botonStart)
+        val bAzul: Button = findViewById(R.id.botonAzul)
+        val bAmarillo: Button = findViewById(R.id.botonAmarillo)
+        val bRojo: Button = findViewById(R.id.botonRojo)
+        val bVerde: Button = findViewById(R.id.botonVerde)
+        val textRonda: TextView = findViewById(R.id.textViewRonda)
     }
+    fun mostrarRonda() {}
+    fun ejecutarSecuencia() {}
+    fun comprobarSecuencia(){}
+
 }
